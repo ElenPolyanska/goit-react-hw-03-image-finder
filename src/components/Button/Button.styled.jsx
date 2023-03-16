46b770be371852bd11996fe137915cfd55491e6d
ElenPolyanska <evp387@gmail.com>
2023-03-16 11:11:40 +0200

    upd btn stl

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -20,13 +20,14 @@ export const LoadBtn = styled.button`
   line-height: 24px;
   font-style: normal;
   font-weight: 500;
-  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-
-  :hover,
-  :focus {
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+   rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+  /* :focus {
+    background-color: #408482;
+  } */
+  :hover
+  {
     background-color: #303f9f;
   }
 `;
