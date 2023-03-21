6e10154790c8ac69d9b71c25886f90602088eb6d
ElenPolyanska <evp387@gmail.com>
2023-03-21 21:12:07 +0200

    uhooo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export class App extends Component {
     page: 1,
     images: [],
     isMore: false,
-    status: 'idle',
+    isLoading: false,
   };
 
   async componentDidUpdate(prevProps, prevState) {
@@ -24,61 +24,55 @@ export class App extends Component {
       prevState.page !== this.state.page
     ) {
       try {
+        this.setState({ isLoading: true });
         const res = await fetchImages(this.state.value, this.state.page);
-        this.setState({ status: 'pending' });
+        this.setState({ isLoading: false });
 
         if (res.hits.length === 0) {
-          this.setState({ status: 'idle' });
           toast.info(
             `Вибачте, але за запитом ${this.state.value} нічого не знайдено`
           );
         }
 
-        if (this.state.page === 1 && res.hits.length !== 0) {
-          toast.success(`Знайдено ${res.totalHits} зображень`);
-          this.setState({ images: res.hits, status: 'resolved' });
-        } else {
+        if (res.hits.length !== 0) {
           this.setState(prevState => {
             return {
               images: [...prevState.images, ...res.hits],
-              status: 'resolved',
             };
           });
         }
-     
-        if (res.hits.length === 12 && res.totalHits > 12 ) {
+        if (this.state.page === 1 && res.totalHits) {
+          toast.success(`Знайдено ${res.totalHits} зображень`);
+        }
+
+        if (res.hits.length === 12 && res.totalHits > 12) {
           this.setState({ isMore: true });
         } else {
           this.setState({ isMore: false });
         }
       } catch (error) {
         toast.info('Шкода, щось пішло не так. Спробуйте ще раз');
-        this.setState({
-          status: 'idle',
-        });
       }
     }
   }
 
   handleSubmit = value => {
-    this.setState({ value, page: 1 });
+    this.setState({ value, page: 1, images: [] });
   };
 
   loadMore = () => {
-    this.setState({ status: 'resolved' });
     this.setState(prevState => ({
       page: prevState.page + 1,
     }));
   };
 
   render() {
-    const { images, isMore, status } = this.state;
-    if (status === 'pending') {
-      return <Loader />;
-    }
+    const { images, isMore, isLoading } = this.state;
+
     return (
       <Box>
         <Searchbar handleSubmit={this.handleSubmit} />
+        {isLoading && <Loader />}
         <ImageGallery images={images} />
         {isMore && <Button onClick={this.loadMore} />}
         <ToastContainer autoClose={2000} theme={'dark'} />
